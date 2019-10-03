583e839504638c660369b3ca0cf8934cf0bde370
mac <mohammed.ali.chherawalla@gmail.com>
2019-10-03 12:46:45 +0530

    typo

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const genBaseConfig = require('../internals/webpack/webpack.base.babel.js')
 module.exports = ({ config }) => {
-  const defualtConfig = genBaseConfig(config)
+  const defaultConfig = genBaseConfig(config)
   config.resolve.modules.push('app')
   config.resolve.alias = config.resolve.alias
   config.resolve.extensions.push('.js', '.jsx', '.react.js')
 
-  config.module.rules.push(...defualtConfig.module.rules)
+  config.module.rules.push(...defaultConfig.module.rules)
   return config
 };
