00816e7a7d873c1a1b5588098f99e31796cb02d6
mac <mohammed.ali.chherawalla@gmail.com>
2019-10-03 06:01:45 +0530

    [skip ci] Updates

--- sw.js
+++ sw.js
@@ -49,7 +49,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "10/3/2019, 5:58:59 AM",
+  "version": "10/3/2019, 6:01:28 AM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
