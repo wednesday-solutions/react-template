1d7514ce0b906f97acba133cad568dae819a39e1
rohit-wednesday <rohit.tayal@wednesday.is>
2020-02-18 16:55:22 +0530

    Added props to router and some other minor changes

--- app/containers/App/tests/index.test.js
+++ app/containers/App/tests/index.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { renderWithIntl } from '@utils/testUtils';
+import { renderProvider } from '@utils/testUtils';
 
 describe('<App /> container tests', () => {
   const App = require('../index').default;
   const history = createMemoryHistory();
   history.push('/');
-  const { container } = renderWithIntl(
+  const { container } = renderProvider(
     <Router history={history}>
       <App />
     </Router>
