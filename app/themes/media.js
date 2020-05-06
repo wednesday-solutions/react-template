ceafc3c29c68c68bc553d296c5314c0718f9f71f
Mohammed Ali Chherawalla <mohammed.ali.chherawalla@gmail.com>
2020-05-06 14:51:14 +0530

    Create media.js

---
+++ app/themes/media.js
@@ -0,0 +1,33 @@
+import { css } from 'styled-components';
+const ScreenSizes = {
+  DESKTOP: 992,
+  TABLET: 768,
+  PHONE: 320
+};
+const sizes = {
+  desktop: ScreenSizes.DESKTOP,
+  tablet: ScreenSizes.TABLET,
+  mobile: ScreenSizes.PHONE
+};
+// iterate through sizes and create a media template
+export const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = {
+    min: args =>
+      css`
+        @media (min-width: ${sizes[label] / 16}em) {
+          ${css([args])};
+        }
+      `
+        .join('')
+        .replace(' ', ''),
+    max: args =>
+      css`
+        @media (max-width: ${sizes[label] / 16}em) {
+          ${css([args])};
+        }
+      `
+        .join('')
+        .replace(' ', '')
+  };
+  return acc;
+}, {});
