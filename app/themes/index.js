c6688ef8f9362c164b83d95252e3a853f6a16894
Mac <mohammed.ali.chherawalla@gmail.com>
2020-05-07 21:32:01 +0530

    adding styles

--- app/themes/index.js
+++ app/themes/index.js
@@ -1,5 +1,7 @@
 import colors from './colors';
 import fonts from './fonts';
 import images from './images';
+import media from './media';
+import styles from './styles';
 
-export { colors, fonts, images };
+export { colors, fonts, images, media, styles };
---
+++ app/themes/styles.js
@@ -0,0 +1,147 @@
+import { css } from 'styled-components';
+import { colors } from '@app/themes';
+
+const configureFlex = (
+  direction = 'row',
+  justifyContent = 'center',
+  alignItems = 'center',
+  alignContent = 'center',
+  flexBasis = 0,
+  flexGrow = 1,
+  flexShrink = 0
+) => css`
+  ${direction === 'row' ? row() : column()}
+  flex-direction: ${direction};
+  justify-content: ${justifyContent};
+  align-items: ${alignItems};
+  align-content: ${alignContent};
+  flex-basis: ${flexBasis};
+  flex-grow: ${flexGrow};
+  flex-shrink: ${flexShrink};
+`;
+
+const row = () => css`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+`;
+const column = () => css`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+`;
+
+const rowCenter = () => css`
+  ${configureFlex('row', 'center', 'center', 'center')};
+`;
+
+const unequalColumns = () => css`
+  ${configureFlex('column', '', '', '', 0, 0, 0)};
+`;
+
+const height = (height = 4) => css`
+  height: ${height}rem;
+`;
+
+const top = (marginTop = 0) =>
+  css`
+    margin-top: ${marginTop}rem;
+  `;
+
+const bottom = (marginBottom = 0) =>
+  css`
+    margin-bottom: ${marginBottom}rem;
+  `;
+
+const left = (marginLeft = 0) =>
+  css`
+    margin-left: ${marginLeft}rem;
+  `;
+
+const right = (marginRight = 0) =>
+  css`
+    margin-right: ${marginRight}rem;
+  `;
+
+const vertical = (verticalMargin = 0) => css`
+  margin-top: ${verticalMargin}rem;
+  margin-bottom: ${verticalMargin}rem;
+`;
+
+const horizontal = (horizontalMargin = 0) => css`
+  margin-left: ${horizontalMargin}rem;
+  margin-right: ${horizontalMargin}rem;
+`;
+
+const borderRadiusBottom = (bottomRadius = 0) => css`
+  border-bottom-left-radius: ${bottomRadius}px;
+  border-bottom-right-radius: ${bottomRadius}px;
+`;
+
+const borderRadiusTop = (topRadius = 0) => css`
+  border-top-left-radius: ${topRadius}px;
+  border-top-right-radius: ${topRadius}px;
+`;
+
+const borderRadius = radius =>
+  css`
+    border-radius: ${radius + `${typeof radius === `string` ? `;` : `px`}`};
+  `;
+
+const borderWithRadius = (
+  width = 1,
+  type = 'solid',
+  color = '#ccc',
+  radius = 0
+) =>
+  css`
+    border: ${width}px ${type} ${color};
+    ${borderRadius(radius)}
+  `;
+
+const boxShadow = (
+  hOffset = 0,
+  vOffset = 0,
+  blur = 0,
+  spread = 0,
+  color = '#ccc'
+) =>
+  css`
+    box-shadow: ${hOffset}px ${vOffset}px ${blur}px ${spread}px ${color};
+  `;
+
+const primaryBackgroundColor = () =>
+  css`
+    background-color: ${colors.accent};
+  `;
+
+const zIndex = (z = 1) => {
+  css`
+    z-index: ${z};
+  `;
+};
+
+export default {
+  height,
+  zIndex,
+  margin: {
+    top,
+    bottom,
+    right,
+    left,
+    vertical,
+    horizontal
+  },
+  borderWithRadius,
+  borderRadius,
+  borderRadiusBottom,
+  borderRadiusTop,
+  primaryBackgroundColor,
+  flexConfig: {
+    row,
+    column,
+    rowCenter,
+    unequalColumns
+  },
+  boxShadow
+};
