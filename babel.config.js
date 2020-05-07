09a6aa881ded06787bc8778a85b0dab92698c6af
Mac <mohammed.ali.chherawalla@gmail.com>
2020-05-07 21:51:21 +0530

    updating babel config

--- babel.config.js
+++ babel.config.js
@@ -21,20 +21,20 @@ module.exports = {
         'transform-react-remove-prop-types',
         '@babel/plugin-transform-react-inline-elements',
         '@babel/plugin-transform-react-constant-elements',
-        ['import', { libraryName: 'antd', style: 'css' }]
+        ['import', { libraryName: 'antd', style: true }]
       ]
     },
     dev: {
-      plugins: [['import', { libraryName: 'antd', style: 'css' }]]
+      plugins: [['import', { libraryName: 'antd', style: true }]]
     },
     development: {
-      plugins: [['import', { libraryName: 'antd', style: 'css' }]]
+      plugins: [['import', { libraryName: 'antd', style: true }]]
     },
     test: {
       plugins: [
         '@babel/plugin-transform-modules-commonjs',
         'dynamic-import-node',
-        ['import', { libraryName: 'antd', style: 'css' }]
+        ['import', { libraryName: 'antd', style: true }]
       ]
     }
   }
