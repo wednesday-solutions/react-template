c2baaa25d00c6e3d53d2529daa427f35b07c9c1a
Mac <mohammed.ali.chherawalla@gmail.com>
2020-05-07 23:15:54 +0530

    update storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,33 @@
-const path = require('path')
-const genBaseConfig = require('../internals/webpack/webpack.base.babel.js')
+const path = require('path');
+const genBaseConfig = require('../internals/webpack/webpack.base.babel.js');
 module.exports = ({ config }) => {
   // hack cause smart knobs is not working on production
-  process.env.NODE_ENV = 'development'
+  process.env.NODE_ENV = 'development';
 
-  const defaultConfig = genBaseConfig(config)
-  config.resolve.modules.push('app')
-  config.resolve.alias = defaultConfig.resolve.alias
-  config.resolve.extensions.push('.js', '.jsx', '.react.js')
-  return config
+  config.resolve.alias = genBaseConfig(config).resolve.alias;
+
+  config.module.rules.push({
+    test: /\.less$/,
+    use: [
+      {
+        loader: 'style-loader' // creates style nodes from JS strings
+      },
+      {
+        loader: 'css-loader'
+      },
+      {
+        loader: 'less-loader',
+        options: {
+          lessOptions: {
+            javascriptEnabled: true,
+            modifyVars: {}
+          }
+        }
+      }
+    ],
+    include: path.resolve(__dirname, '../')
+  });
+  config.resolve.modules.push('app');
+  config.resolve.extensions.push('.js', '.jsx', '.react.js');
+  return config;
 };
