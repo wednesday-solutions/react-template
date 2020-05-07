023e237860e70556ddcd9a30e1b50d248d1bac2e
Mac <mohammed.ali.chherawalla@gmail.com>
2020-05-07 21:29:14 +0530

    adding if

---
+++ app/components/If/index.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line
+import React from 'react';
+import Proptypes from 'prop-types';
+const If = props => (props.condition ? props.children : props.otherwise);
+If.propsTypes = {
+  condition: Proptypes.bool,
+  otherwise: Proptypes.oneOfType([
+    Proptypes.arrayOf(Proptypes.node),
+    Proptypes.node
+  ]),
+  children: Proptypes.oneOfType([
+    Proptypes.arrayOf(Proptypes.node),
+    Proptypes.node
+  ])
+};
+If.defaultProps = {
+  otherwise: null
+};
+export default If;
