c200bd63e61619e9dd90bda04b285333dcd208e2
Mac <mohammed.ali.chherawalla@gmail.com>
2020-05-07 21:32:58 +0530

    updating text

--- app/components/Text/index.js
+++ app/components/Text/index.js
@@ -8,25 +8,30 @@ import React, { memo } from 'react';
 import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
 import { PropTypes } from 'prop-types';
+import If from '@components/If';
 
-const StyledText = styled.div`
+const StyledText = styled.p`
   && {
     ${props => props.marginBottom && `margin-bottom: ${props.marginBottom}px;`};
   }
 `;
-export const Text = ({ id, marginBottom }) => (
-  <StyledText data-testid="text" marginBottom={marginBottom}>
-    {id && <FormattedMessage id={id} />}
+export const Text = ({ text, id, marginBottom, values, ...otherProps }) => (
+  <StyledText data-testid="text" marginBottom={marginBottom} {...otherProps}>
+    <If condition={id} otherwise={text}>
+      <FormattedMessage id={id} values={values} />
+    </If>
   </StyledText>
 );
 
 Text.propTypes = {
   id: PropTypes.string,
-  marginBottom: PropTypes.number
+  marginBottom: PropTypes.number,
+  values: PropTypes.object,
+  text: PropTypes.string
 };
 
 Text.defaultProps = {
-  id: 'some_text'
+  values: {}
 };
 
 const TextComponent = memo(Text);
