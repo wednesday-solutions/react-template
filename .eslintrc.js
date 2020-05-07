295f09c4c5d27a82ed43132deb00c0ff03cffabc
Mac <mohammed.ali.chherawalla@gmail.com>
2020-05-07 21:58:23 +0530

    updating eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,39 +2,36 @@ const fs = require('fs');
 const path = require('path');
 
 const prettierOptions = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8')
 );
 
 module.exports = {
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "es6": true,
-    "amd": true,
-    "jest/globals": true
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+    amd: true,
+    'jest/globals': true
   },
-  "plugins": [
-    "react",
-    'redux-saga',
-    'react-hooks',
-    "jest"
-  ],
+  plugins: ['react', 'redux-saga', 'react-hooks', 'jest'],
 
-  "extends": [
+  extends: [
     'prettier',
     'prettier/react',
-    "prettier-standard",
-    "plugin:react/recommended"
+    'prettier-standard',
+    'plugin:react/recommended'
   ],
-  "rules": {
+  rules: {
     'prettier/prettier': ['error', prettierOptions],
-    "import/no-webpack-loader-syntax": 0,
-    "react/display-name": 0
+    'import/no-webpack-loader-syntax': 0,
+    'react/display-name': 0,
+    curly: ['error', 'all'],
+    'no-console': ['error', { allow: ['error'] }]
   },
-  "globals": {
-    "GLOBAL": false,
-    "it": false,
-    "expect": false,
-    "describe": false
+  globals: {
+    GLOBAL: false,
+    it: false,
+    expect: false,
+    describe: false
   }
 };
