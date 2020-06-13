706c2f862020a3082f8577e79ad9a74bbab183ae
Mac <mohammed.ali.chherawalla@gmail.com>
2020-06-14 00:52:38 +0530

    importing .less in global-styles

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-@import '~antd/dist/antd.css';
+@import 'antd/dist/antd.less';
   html,
   body {
     height: 100vh;
