f0b53568efecc2460bde19139cca6134c7233241
amitWaghamareWednesDay <amit@wednesday.is>
2020-07-03 14:34:10 +0530

    play icon added

--- app/containers/SongList/index.js
+++ app/containers/SongList/index.js
@@ -9,6 +9,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
+import { PlayCircleOutlined } from '@ant-design/icons';
 
 // import styled from 'styled-components'
 import { Card, Skeleton } from 'antd';
@@ -16,13 +17,46 @@ import If from '@app/components/If/index';
 import { selectShowLoader } from '../ItunesAppContainer/selectors';
 
 const CustomCard = styled(Card)`
-  .songImg {
-    max-height: 200px;
-  }
   && {
     cursor: pointer;
     margin-top: 10px;
-    min-height: 200px;
+  }
+`;
+
+const CustomPlayCircleOutlined = styled(PlayCircleOutlined)`
+  && {
+    font-size: 50px;
+    color: red;
+    margin-left: 40%;
+    margin-top: 40%;
+  }
+`;
+const StyledImage = styled.div`
+  .thumbNail {
+    width: 100%;
+    height: 100%;
+    display: none;
+  }
+  .thumbNailContainer {
+    width: 100%;
+    height: 100%;
+  }
+
+  .thumbNailContainer:hover .thumbNail {
+    display: inline;
+  }
+
+  .thumbNailContainer:hover {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  && {
+    display: block;
+    background: url(${props => props.url});
+    width: 100%;
+    height: 389px;
+    background-size: 100%;
+    background-repeat: no-repeat;
   }
 `;
 
@@ -41,8 +75,19 @@ export function SongList({ songs = [], showLoader }) {
                 </If>
                 <If condition={!showLoader}>
                   <CustomCard
-                    onClick={() => window.open(songItem?.collectionViewUrl, '_blank')}
-                    cover={<img alt="example" className="songImg" src={songItem?.artworkUrl100} />}
+                    cover={
+                      <StyledImage url={songItem?.artworkUrl100}>
+                        <div className="thumbNailContainer">
+                          <div className="thumbNail">
+                            <CustomPlayCircleOutlined
+                              onClick={() => {
+                                window.open(songItem?.previewUrl, '_blank');
+                              }}
+                            />
+                          </div>
+                        </div>
+                      </StyledImage>
+                    }
                     title={songItem?.trackName}
                     bordered={true}
                   >
