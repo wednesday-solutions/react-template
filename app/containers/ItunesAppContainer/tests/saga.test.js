44eb0cc12af05abfe82bbb1848bb2add4626bec9
amitWaghamareWednesDay <amit@wednesday.is>
2020-07-06 15:27:31 +0530

    minor changes

--- app/containers/ItunesAppContainer/tests/saga.test.js
+++ app/containers/ItunesAppContainer/tests/saga.test.js
@@ -8,34 +8,21 @@ import itunesAppContainerSaga, { requestSearchSong } from '../saga';
 import { itunesAppContainerTypes, itunesAppContainerCreators } from '../reducer';
 import { getSongsApi } from '@app/services/ituneSongsApi';
 
-const { setSongs, setLoader } = itunesAppContainerCreators;
+const { setLoader } = itunesAppContainerCreators;
 
 describe('ItunesAppContainer saga tests', () => {
   it('should start task to watch for REQUEST_SEARCH_SONG action', () => {
     const generator = itunesAppContainerSaga();
-
     expect(generator.next().value).toEqual(takeLatest(itunesAppContainerTypes.REQUEST_SEARCH_SONG, requestSearchSong));
   });
-  // it('should dispatch action SET_LOADER as true', () => {
-  //   const generator = requestSearchSong();
-  //   expect(generator.next().value).toEqual(put(setLoader(true)));
-  // });
-  // it('should call getSongsApi', () => {
-  //   const generator = requestSearchSong({ artistName: 'sample' });
-  //   generator.next();
-  //   expect(generator.next().value).toEqual(call(getSongsApi, 'sample'));
-  // });
-  // it('should dispatch action SET_LOADER as false', () => {
-  //   const generator = requestSearchSong({ artistName: 'sample' });
-  //   generator.next();
-  //   generator.next();
-  //   expect(generator.next().value).toEqual(put(setLoader(false)));
-  // });
-  // it('should dispatch SET_SONGS action', () => {
-  //   const generator = requestSearchSong({ artistName: 'sample' });
-  //   generator.next();
-  //   generator.next();
-  //   generator.next([{ trackName: 'sample song' }]);
-  //   expect(generator.next().value).toEqual(put(setSongs([{ trackName: 'sample song' }])));
-  // });
+
+  it('should dispatch action SET_LOADER as true', () => {
+    const generator = requestSearchSong();
+    expect(generator.next().value).toEqual(put(setLoader(true)));
+  });
+  it('should call getSongsApi', () => {
+    const generator = requestSearchSong({ artistName: 'sample' });
+    generator.next();
+    expect(generator.next().value).toEqual(call(getSongsApi, 'sample'));
+  });
 });
