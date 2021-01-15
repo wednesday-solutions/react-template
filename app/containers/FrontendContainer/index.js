d1e01eb66c916b3ed9573ea3ef8af6cc84a531a1
Kanika1799 <kanika17agrawal99@gmail.com>
2021-01-15 18:19:24 +0530

    changes in index.test.js and index.js

--- app/containers/FrontendContainer/index.js
+++ app/containers/FrontendContainer/index.js
@@ -4,18 +4,15 @@
  *
  */
 
-import React, { useEffect, memo, useState } from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { injectIntl, FormattedMessage as T } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import { useInjectSaga } from '@utils/injectSaga';
 import styled from 'styled-components';
-import debounce from 'lodash/debounce';
-import isEmpty from 'lodash/isEmpty';
-import get from 'lodash/get';
-import { Card, message, Button, Space, Input, Row, Col } from 'antd';
+import { Row, Col } from 'antd';
 import { selectFrontendContainer, selectDashData, selectDashError, selectDashName } from './selectors';
 import { frontendContainerCreators } from './reducer';
 import CardComponent from './CardComponent';
@@ -23,11 +20,7 @@ import CalendarComponent from './Calendar';
 import Pie from './Pie';
 import AmountCard from './AmountCard';
 import saga from './saga';
-const { Search } = Input;
 
-const DetailsCard = styled(Card)`
-  margin: 20px 20px;
-`;
 const Container = styled.div`
   && {
     display: flex;
@@ -38,86 +31,10 @@ const Container = styled.div`
     padding: ${props => props.padding}px;
   }
 `;
-const CustomDiv = styled.div`
-  width: 100%;
-  font-weight: bolder;
-  font-size: 20px;
-`;
-
-const CustomSpace = styled(Space)`
-  margin: ${props => props.margin}px;
-`;
 export function FrontendContainer({ dispatchDashboard, dashData = {}, dashError = null, dashName, intl }) {
   useInjectSaga({ key: 'frontendContainer', saga });
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const loaded = get(dashData, 'name', dashError);
-    if (loading && loaded) {
-      setLoading(false);
-    }
-  }, [dashData]);
-
-  useEffect(() => {
-    if (dashName && !dashData.main) {
-      dispatchDashboard(dashName);
-      setLoading(true);
-    }
-  }, []);
-  const handleOnChange = wName => {
-    if (!isEmpty(wName)) {
-      dispatchDashboard(wName);
-      setLoading(true);
-    }
-  };
-  const debouncedHandleOnChange = debounce(handleOnChange, 400);
-  const renderDashboardList = () => {
-    const name = get(dashData, 'name', null);
-    return (
-      name && (
-        <DetailsCard>
-          <CustomDiv>
-            <T id="city_name" values={{ name: dashData.name }} />
-          </CustomDiv>
-          <CustomDiv>
-            <T id="city_timezone" values={{ timezone: dashData.timezone }} />
-          </CustomDiv>
-          <CustomDiv>
-            <T id="city_temperature" values={{ temperature: dashData.main.temp }} />
-          </CustomDiv>
-          <CustomDiv>
-            <T id="city_description" values={{ description: dashData.weather[0].description }} />
-          </CustomDiv>
-        </DetailsCard>
-      )
-    );
-  };
-  const renderErrorState = () => {
-    const error = () => {
-      message.error(intl.formatMessage({ id: 'city_error' }));
-    };
-    const name = get(dashData, 'name', null);
-    if (dashError) {
-      return (
-        !loading &&
-        !name && (
-          <CustomSpace>
-            <Button onClick={error}>Error</Button>
-          </CustomSpace>
-        )
-      );
-    }
-  };
-
   return (
     <Container>
-      <Search
-        data-testid="search-bar"
-        defaultValue={dashName}
-        type="text"
-        onChange={evt => debouncedHandleOnChange(evt.target.value)}
-        onSearch={searchText => debouncedHandleOnChange(searchText)}
-      />
-
       <CardComponent />
       <Row>
         <Col>
@@ -128,9 +45,6 @@ export function FrontendContainer({ dispatchDashboard, dashData = {}, dashError 
         </Col>
       </Row>
       <CalendarComponent />
-
-      {renderDashboardList()}
-      {renderErrorState()}
     </Container>
   );
 }
--- app/containers/FrontendContainer/tests/index.test.js
+++ app/containers/FrontendContainer/tests/index.test.js
@@ -6,8 +6,7 @@
  */
 
 import React from 'react';
-import { timeout, renderProvider } from '@utils/testUtils';
-import { fireEvent } from '@testing-library/dom';
+import { renderProvider } from '@utils/testUtils';
 import { FrontendContainerTest as FrontendContainer } from '../index';
 
 describe('<FrontendContainer /> container tests', () => {
@@ -19,23 +18,5 @@ describe('<FrontendContainer /> container tests', () => {
   it('should render and match the snapshot', () => {
     const { baseElement } = renderProvider(<FrontendContainer dispatchDashboard={submitSpy} />);
     expect(baseElement).toMatchSnapshot();
-  });
-  it('should call', async () => {
-    const getDashboardSpy = jest.fn();
-    const { getByTestId } = renderProvider(<FrontendContainer dispatchDashboard={getDashboardSpy} />);
-    fireEvent.change(getByTestId('search-bar'), {
-      target: { value: 'a' }
-    });
-    await timeout(500);
-    expect(getDashboardSpy).toBeCalled();
-  });
-
-  it('should call dispatchDashboard on change', async () => {
-    const { getByTestId } = renderProvider(<FrontendContainer dispatchDashboard={submitSpy} />);
-    fireEvent.change(getByTestId('search-bar'), {
-      target: { value: 'some city' }
-    });
-    await timeout(500);
-    expect(submitSpy).toBeCalled();
   });
 });
