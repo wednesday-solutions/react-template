c23d3a8c73dd4e3d3609061143cd65d476157737
Tousif <tousifislampersonal@outlook.com>
2021-04-14 09:01:47 +0530

    reposError holding an string

--- app/containers/HomeContainer/index.js
+++ app/containers/HomeContainer/index.js
@@ -159,7 +159,7 @@ HomeContainer.propTypes = {
     incompleteResults: PropTypes.bool,
     items: PropTypes.array
   }),
-  reposError: PropTypes.object,
+  reposError: PropTypes.string,
   repoName: PropTypes.string,
   history: PropTypes.object,
   maxwidth: PropTypes.number,
