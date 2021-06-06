87b0107d4634d582800ef793ba9a4b44194c1291
tarun9142 <79507487+tarun9142@users.noreply.github.com>
2021-06-06 11:59:10 +0530

    Update webpack.base.babel.js

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -26,7 +26,7 @@ module.exports = options => ({
     {
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
-      publicPath: process.env.NODE_ENV === 'production' ? '/react-template/' : '/'
+      publicPath:'/'
     },
     options.output
   ), // Merge with env dependent settings
