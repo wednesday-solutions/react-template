3d45a7f910ab89aae50343ad89889dc2bf7a3c54
tarun9142 <79507487+tarun9142@users.noreply.github.com>
2021-06-06 12:01:54 +0530

    Update history.js

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,4 +1,4 @@
 import { createBrowserHistory } from 'history';
 const baseUrl = process.env.NODE_ENV === 'production' ? '/react-template' : '/';
-const history = createBrowserHistory({ basename: baseUrl });
+const history = createBrowserHistory();
 export default history;
