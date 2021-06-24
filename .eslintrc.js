bef44932fb95a805584906d0eec5763bfb5bf38b
Rohan <apple@Apples-MacBook-Pro.local>
2021-06-24 13:32:40 +0530

    Upgrade Eslint to v7.29.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const prettierOptions = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8')
-);
+const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'));
 
 module.exports = {
   parser: 'babel-eslint',
@@ -11,22 +9,20 @@ module.exports = {
     browser: true,
     es6: true,
     amd: true,
-    'jest/globals': true
+    'jest/globals': true,
+    commonjs: true
   },
   plugins: ['react', 'redux-saga', 'react-hooks', 'jest'],
 
-  extends: [
-    'prettier',
-    'prettier/react',
-    'prettier-standard',
-    'plugin:react/recommended'
-  ],
+  extends: ['prettier', 'prettier/react', 'prettier-standard', 'plugin:react/recommended', 'eslint:recommended'],
   rules: {
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
     'prettier/prettier': ['error', prettierOptions],
     'import/no-webpack-loader-syntax': 0,
     'react/display-name': 0,
     curly: ['error', 'all'],
-    'no-console': ['error', { allow: ['error'] }]
+    'no-console': ['error', { allow: ['error'] }],
+    'key-spacing': [2, { beforeColon: false, afterColon: true }]
   },
   globals: {
     GLOBAL: false,
