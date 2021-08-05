b2e598648bd30114d31f678d64a5e3628723bd31
Mac <mohammed.ali.chherawalla@gmail.com>
2021-08-05 11:57:32 +0530

    update prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,13 +16,11 @@ module.exports = {
 
   extends: ['prettier', 'prettier/react', 'prettier-standard', 'plugin:react/recommended', 'eslint:recommended'],
   rules: {
-    'prettier/prettier': [prettierOptions],
     'import/no-webpack-loader-syntax': 0,
     'react/display-name': 0,
     curly: ['error', 'all'],
     'no-console': ['error', { allow: ['error'] }],
-    'key-spacing': [2, { beforeColon: false, afterColon: true }],
-    'arrow-parens': ['error', 'as-needed']
+    'prettier/prettier': ['error', prettierOptions]
   },
   globals: {
     GLOBAL: false,
