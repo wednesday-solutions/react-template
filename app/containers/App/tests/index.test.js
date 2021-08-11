b0e37b415782d170ef8e257db5b76f6d52b6cb4d
Mac <mohammed.ali.chherawalla@gmail.com>
2021-08-11 20:18:05 +0530

    update App test

--- app/containers/App/tests/index.test.js
+++ app/containers/App/tests/index.test.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import { renderWithIntl } from '@utils/testUtils';
 import App from '../index';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('<App /> container tests', () => {
   it('should render and match the snapshot', () => {
-    const { container } = renderWithIntl(App);
+    const { container } = renderWithIntl(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
     expect(container).toMatchSnapshot();
   });
 });
--- jest.setup.js
+++ jest.setup.js
@@ -1,25 +1,28 @@
 import '@testing-library/jest-dom';
 
-jest.mock('react-router-dom', () => ({
-  __esModule: true,
-  useLocation: jest.fn().mockReturnValue({
-    pathname: '/',
-    search: '',
-    hash: '',
-    state: null,
-    key: '5nvxpbdafa'
-  }),
-  useHistory: jest.fn().mockReturnValue({
-    length: 2,
-    action: 'POP',
-    location: {
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom');
+  return {
+    __esModule: true,
+    ...originalModule,
+    useLocation: jest.fn().mockReturnValue({
       pathname: '/',
       search: '',
-      hash: ''
-    }
-  })
-}));
-
+      hash: '',
+      state: null,
+      key: '5nvxpbdafa'
+    }),
+    useHistory: jest.fn().mockReturnValue({
+      length: 2,
+      action: 'POP',
+      location: {
+        pathname: '/',
+        search: '',
+        hash: ''
+      }
+    })
+  };
+});
 Object.defineProperty(window, 'matchMedia', {
   value: jest.fn(() => {
     return {
