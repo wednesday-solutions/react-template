f0f910919b50a89d74819bb23cce9287d0e92fce
Abhishek Mishra <abhishek@wednesday.is>
2021-09-16 12:30:03 +0530

    Fix: media.js, breakpoints for desktop, mobile and tablet

--- app/themes/media.js
+++ app/themes/media.js
@@ -1,21 +1,15 @@
 import { generateMedia } from 'styled-media-query';
 
 const ScreenBreakPoints = {
-  XS: 480,
-  SM: 576,
+  SM: 320,
   MD: 768,
-  LG: 992,
-  XL: 1366,
-  XXL: 1600
+  LG: 992
 };
 
 const media = generateMedia({
-  xs: `${ScreenBreakPoints.XS / 16}em`,
   sm: `${ScreenBreakPoints.SM / 16}em`,
   md: `${ScreenBreakPoints.MD / 16}em`,
-  lg: `${ScreenBreakPoints.LG / 16}em`,
-  xl: `${ScreenBreakPoints.XL / 16}em`,
-  xxl: `${ScreenBreakPoints.XXL / 16}em`
+  lg: `${ScreenBreakPoints.LG / 16}em`
 });
 
 export default media;
