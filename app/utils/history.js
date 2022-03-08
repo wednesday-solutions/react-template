3d69926c58246ca0a0bc53d20553248cf14de0fe
Aseer KT <aseer@wednesday.is>
2022-03-08 12:31:13 +0530

    chore: reduce nestings in history `getBaseUrl`

--- app/utils/history.js
+++ app/utils/history.js
@@ -7,49 +7,47 @@ const routes = Object.keys(routeConstants);
 
 /** @param {string} pathname */
 export function getBaseUrl(pathname) {
-  if (process.env.NODE_ENV === 'development') {
-    return '';
-  }
-
   if (isProd()) {
     // GH Pages
     return '/react-template';
   }
 
+  if (!isUAT()) {
+    return '';
+  }
+
   let baseURL = '';
 
-  if (isUAT()) {
-    let isMatchedOnce = false;
-    routes.forEach((routeKey) => {
-      /** @type {string} */
-      const route = routeConstants[routeKey].route;
+  let isMatchedOnce = false;
+  routes.forEach((routeKey) => {
+    /** @type {string} */
+    const route = routeConstants[routeKey].route;
 
-      if (pathname.includes(route)) {
-        if (pathname.endsWith(route)) {
-          baseURL = pathname.substring(0, pathname.length - route.length);
-          isMatchedOnce = true;
-        } else if (pathname.endsWith(`${route}/`)) {
-          baseURL = pathname.substring(0, pathname.length - route.length - 1);
-          isMatchedOnce = true;
-        }
-      } else if (route.includes(':')) {
-        const routeSplitArr = route.split('/').filter((val) => val !== '');
-        const match = '/' + routeSplitArr[0];
-        const matchLastIndex = pathname.lastIndexOf(match);
-        const pathToMatch = pathname.substring(matchLastIndex);
-        const isMatch = matchPath(pathToMatch, {
-          path: route,
-          exact: true
-        });
-        if (isMatch) {
-          baseURL = pathname.substring(0, matchLastIndex);
-          isMatchedOnce = true;
-        }
+    if (pathname.includes(route)) {
+      if (pathname.endsWith(route)) {
+        baseURL = pathname.substring(0, pathname.length - route.length);
+        isMatchedOnce = true;
+      } else if (pathname.endsWith(`${route}/`)) {
+        baseURL = pathname.substring(0, pathname.length - route.length - 1);
+        isMatchedOnce = true;
       }
-    });
-    if (!isMatchedOnce) {
-      baseURL = pathname;
+    } else if (route.includes(':')) {
+      const routeSplitArr = route.split('/').filter((val) => val !== '');
+      const match = '/' + routeSplitArr[0];
+      const matchLastIndex = pathname.lastIndexOf(match);
+      const pathToMatch = pathname.substring(matchLastIndex);
+      const isMatch = matchPath(pathToMatch, {
+        path: route,
+        exact: true
+      });
+      if (isMatch) {
+        baseURL = pathname.substring(0, matchLastIndex);
+        isMatchedOnce = true;
+      }
     }
+  });
+  if (!isMatchedOnce) {
+    baseURL = pathname;
   }
 
   return baseURL;
