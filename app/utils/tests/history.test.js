192dcf48f2d9ce991e9526de54d1a0002112b771
Aseer KT <aseer@wednesday.is>
2022-03-15 11:23:15 +0530

    chore: update UAT baseUrl for history

--- app/utils/tests/history.test.js
+++ app/utils/tests/history.test.js
@@ -23,8 +23,14 @@ jest.mock('../routeConstants', () => ({
 
 describe('history tests', () => {
   const OLD_ENV = process.env;
+  const oldLocation = window.location;
+
   beforeEach(() => {
     jest.resetModules();
+    delete window.location;
+    window.location = {
+      pathname: '/'
+    };
     process.env = { ...OLD_ENV };
     process.env.ENVIRONMENT_NAME = 'development';
     process.env.NODE_ENV = 'production';
@@ -32,57 +38,30 @@ describe('history tests', () => {
 
   afterAll(() => {
     process.env = OLD_ENV;
+    window.location = oldLocation;
   });
 
   it("should return getBaseUrl '/' if process.env.NODE_ENV is development", () => {
     process.env.NODE_ENV = 'development';
-    const pathname = '/tracks/321';
-    expect(getBaseUrl(pathname)).toBe('');
+    window.location.pathname = '/tracks/321';
+    expect(getBaseUrl()).toBe('');
   });
 
-  it("should return baseURL '/test-relative-path' if process.env.ENVIRONMENT_NAME is 'development' ", () => {
-    const pathname = '/test-relative-path';
-    expect(getBaseUrl(pathname)).toBe(pathname);
+  it('should return baseURL BRANCH_NAME in UAT environment', () => {
+    process.env.BRANCH_NAME = '/feat/test-pr';
+    window.location.pathname = '/feat/test-pr/tracks/123';
+    expect(getBaseUrl()).toBe(process.env.BRANCH_NAME);
+    process.env.BRANCH_NAME = undefined;
   });
 
-  it('should return baseURL /test-relative-path if pathname is /test-relatvie-path/repos', () => {
-    const pathname = '/test-relative-path/repos';
-    expect(getBaseUrl(pathname)).toBe('/test-relative-path');
-  });
-
-  it('should return baseURL /test-relative-path if pathname is /test-relatvie-path/repos/', () => {
-    const pathname = '/test-relative-path/repos/';
-    expect(getBaseUrl(pathname)).toBe('/test-relative-path');
-  });
-
-  it('should reutnr baseURL /test-relative-path if pathname is /test-relative-path/tracks/123', () => {
-    const pathname = '/test-relative-path/tracks/123';
-    expect(getBaseUrl(pathname)).toBe('/test-relative-path');
-  });
-
-  it('should return baseURL /test-relative-path/tracks/123 if pathname is /test-relative-path/tracks/123/tracks', () => {
-    const pathname = '/test-relative-path/tracks/123/tracks';
-    expect(getBaseUrl(pathname)).toBe('/test-relative-path/tracks/123');
-  });
-
-  it('should return baseURL /test-relative-path if pathname is /test-relative-path/artists/123/tracks/321', () => {
-    const pathname = '/test-relative-path/artists/123/tracks/321';
-    expect(getBaseUrl(pathname)).toBe('/test-relative-path');
-  });
-
-  it('should return baseURL /test-relative-path if pathname is /test-relative-path/artists/123/tracks/321/', () => {
-    const pathname = '/test-relative-path/artists/123/tracks/321/';
-    expect(getBaseUrl(pathname)).toBe('/test-relative-path');
-  });
-
-  it('should return baseURL same as pathname if pathname is /test-relative-path/artists/123/track2/321', () => {
-    const pathname = '/test-relative-path/artists/123/track2/321';
-    expect(pathname).toBe(pathname);
+  it('should return baseURL from pathname in UAT environment if BRANCH_NAME in undefined', () => {
+    window.location.pathname = '/feat/test-pr/index.html';
+    expect(getBaseUrl()).toBe('/feat/test-pr');
   });
 
   it('should return baseUrl /react-template if NODE_ENV and ENVIRONMENT_NAME is production', () => {
     process.env.ENVIRONMENT_NAME = 'production';
-    const pathname = '/test-relative-path';
-    expect(getBaseUrl(pathname)).toBe('/react-template');
+    window.location.pathname = '/test-relative-path';
+    expect(getBaseUrl()).toBe('/react-template');
   });
 });
