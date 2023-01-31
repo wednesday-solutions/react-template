f97147416056da9f36c6d17bccf5787dafd59dde
vipinWednesday <vipin@wednesday.is>
2023-01-31 16:15:42 +0530

    fix: accessbility test for components

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -28,6 +28,7 @@ const Logo = styled.img`
 `;
 const Title = styled(T)`
   && {
+    color: ${colors.text};
     margin-bottom: 0;
     ${fonts.dynamicFontSize(fonts.size.xRegular, 1, 0.5)};
     display: flex;
