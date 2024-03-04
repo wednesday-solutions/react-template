c33e75d7d73bae3b968028e1c667a6555e94b412
Mohammed Ali Chherawalla <mohammed.ali.chherawalla@gmail.com>
2024-03-04 22:42:36 +0530

    Rename Loadable.js to loadable.js

--- app/routeConfig.js
+++ app/routeConfig.js
@@ -1,6 +1,7 @@
+import routeConstants from '@utils/routeConstants';
 import NotFound from '@app/containers/NotFoundPage/loadable';
 import HomeContainer from '@app/containers/HomeContainer/loadable';
-import routeConstants from '@utils/routeConstants';
+
 export const routeConfig = {
   repos: {
     component: HomeContainer,
